0943afd7fe0a817daf155499c8c94d70200fb9cb
stanmozolevskiy <stanmozolevskiy@gmail.com>
2023-04-13 14:32:58 -0400

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const axios = require('axios');
-const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+const app = express();
+
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/speechify', async (req, res) => {
