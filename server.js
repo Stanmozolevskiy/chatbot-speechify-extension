7588bd07c3d3ac390e574857c34bb3ae53975df1
stanmozolevskiy <stanmozolevskiy@gmail.com>
2023-04-13 15:10:51 -0400

    openai implamentation

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const axios = require('axios');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const openai = require('openai');
+const openaiApiKey = process.env.OPENAI_API_KEY;
 
 
+openai.apiKey = openaiApiKey;
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -15,9 +18,28 @@ app.post('/speechify', async (req, res) => {
 });
 
 app.post('/gpt', async (req, res) => {
-  // Your ChatGPT and Speechify API integration code will go here
-  console.log("this is the test", req);
-  res.send('GPT endpoint');
+  
+  const inputText = req.body.inputText;
+
+  try{
+    // Call the ChatGPT API with the input text
+    const chatGptResponse = await openai.Completion.create({
+      engine: 'text-davinci-002',
+      prompt: inputText,
+      max_tokens: 150,
+      n: 1,
+      stop: null,
+      temperature: 0.5,
+    });
+    // Extract the generated text from the response
+    const chatGptOutput = chatGptResponse.choices[0].text.trim();  
+    console.log(chatGptOutput)
+
+  } catch (error){
+    console.error('Error:', error);
+    res.status(500).send('An error occurred while processing your request');
+  }
+  
 });
 
 app.listen(PORT, () => {
